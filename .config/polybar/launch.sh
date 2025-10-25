73f95bbd079554a174275b27af63d5ad4a219f08
Anthony Burrow <anthony.r.burrow@gmail.com>
2025-10-25 14:04:19 -0500

    fix polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-polybar main &
+monitors=($(polybar --list-monitors | cut -d: -f1))
+bars=("main" "secondary")
+
+for i in "${!monitors[@]}"; do
+    monitor=${monitors[$i]}
+    bar=${bars[$i]:-main}
+    MONITOR=$monitor polybar --reload "$bar" &
+done
 
 echo "Bar launched..."
